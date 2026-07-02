8509fc41bde4c4f1533c7d5a167edca4282770e2
Johnathan Rocha <johnathanrocha@gmail.com>
2026-07-02 14:33:39 -0300

    feat: add AppRouter for managing application routes

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:go_router/go_router.dart';
+
+import '../../features/trail/presentation/pages/trail_list_page.dart';
+import '../../features/trail/presentation/pages/trail_record_page.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'trails',
+        builder: (context, state) => const TrailListPage(),
+      ),
+      GoRoute(
+        path: '/trails/record',
+        name: 'record-trail',
+        builder: (context, state) => const TrailRecordPage(),
+      ),
+    ],
+  );
+}
